027345dda421ce7827c89a51a32d31d61261fad1
gregor <gjkcme@gmail.com>
2025-08-12 18:59:34 +0200

    Delete lib directory

--- lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient as createSupabaseClient } from '@supabase/supabase-js';
-
-export function createClient() {
-  const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-  const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-  return createSupabaseClient(url, anon);
-}
