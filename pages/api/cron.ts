bb7690b594d2bf4e221b752b1b0b6da23493c965
gregor <gjkcme@gmail.com>
2025-08-12 18:59:41 +0200

    Delete pages directory

--- pages/api/cron.ts
+++
@@ -1,6 +0,0 @@
-export default async function handler(req: any, res: any) {
-  const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000';
-  const r = await fetch(`${baseUrl}/api/fetchTweets`);
-  const j = await r.json().catch(() => ({}));
-  res.status(200).json({ message: 'Cron triggered', result: j });
-}
